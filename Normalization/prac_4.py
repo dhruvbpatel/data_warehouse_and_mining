ac1a7e8aa8f8213027003357c0d42d9f3e042000
dhruvbpatel <dhruvpatel1057@gmail.com>
2020-01-23 12:07:28 +0530

    added normalization

---
+++ Normalization/prac_4.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 22 11:44:40 2020
+
+@author: dhruv
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('data.csv')
+
+
+data.colums
